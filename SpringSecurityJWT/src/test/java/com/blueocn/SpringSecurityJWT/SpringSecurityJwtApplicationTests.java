a86f41780129f253f79086887eb3664fb0f04da7
BlueOCN <robgonzalezmu@gmail.com>
2025-06-05 18:41:58 -0600

    Update - Step 1: Set Up the Project Complete.

---
+++ SpringSecurityJWT/src/test/java/com/blueocn/SpringSecurityJWT/SpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blueocn.SpringSecurityJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
